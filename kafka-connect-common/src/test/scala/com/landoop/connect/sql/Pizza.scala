db5913aa75f1e2b308ac977f3253b2c54e5cec53
Kanstantsin Kamkou <me@2ka.by>
2022-09-28 16:18:35 +0200

    cleanups

--- kafka-connect-common/src/test/scala/com/landoop/connect/sql/Pizza.scala
+++
@@ -1,9 +0,0 @@
-package com.landoop.connect.sql
-
-case class Ingredient(name: String, sugar: Double, fat: Double)
-
-case class Pizza(name: String,
-                 ingredients: Seq[Ingredient],
-                 vegetarian: Boolean,
-                 vegan: Boolean,
-                 calories: Int)
